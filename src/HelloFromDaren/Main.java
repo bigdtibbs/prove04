2938f38639a612e6e2882cf4f7414418847ce9f2
man18008 <meganmanwaring@gbyui.edu>
2020-10-08 16:15:42 -0600

    Added Line "Hello <your team members name>, my name is <your name>."

--- src/HelloFromDaren/Main.java
+++ src/HelloFromDaren/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Daren.");
-        System.out.println("Hello , my name is <your name>.");
+        System.out.println("Hello Daren, my name is Megan.");
     }
 }
