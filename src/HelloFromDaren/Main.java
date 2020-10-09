ae76193214d4a65848395b39cbc4810d5e924744
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-10-09 15:37:15 -0700

    Lee's edits.

--- src/HelloFromDaren/Main.java
+++ src/HelloFromDaren/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Daren.\n");
         System.out.println("Hello Daren, my name is Megan.\n");
+        System.out.println("Hello Daren, my name is Lee.\n");
         System.out.println("Thank you for your time Megan.\n");
         System.out.println("Making changes for the stretch challenge.\n");
 	    System.out.println("This change made in github.\n"); // ")" Added in IntelliJ and recommitted.
