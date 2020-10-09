c8c002ffae5bace6a8e4aa4468e53cedaf466d65
tomtomm <bigdtibbs@gmail.com>
2020-10-08 20:41:48 -0600

    For stretch challenge.

--- src/HelloFromDaren/Main.java
+++ src/HelloFromDaren/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, my name is Daren.");
-        System.out.println("Hello Daren, my name is Megan.");
+        System.out.println("Hello, my name is Daren.\n");
+        System.out.println("Hello Daren, my name is Megan.\n");
+        System.out.println("Thank you for your time Megan.\n");
     }
 }
